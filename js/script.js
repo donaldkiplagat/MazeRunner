cbdb2003b4687f847e940e035c0dad3c283edaf9
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-29 11:40:44 +0300

    jQuery and CSS till level 3 done

--- js/script.js
+++ js/script.js
@@ -1,27 +1,124 @@
-// $(document).ready(function(){
-//   $("#quest1-0").click(function(){
-//     $("#question1").hide();
-//     $("#question2-0").show();
-//   });
-//
-//   $("#quest1-1").click(function(){
-//     $("#question1").hide();
-//     $("#question2-1").show();
-//   });
-//
-//   $("#quest1-2").click(function(){
-//     $("#question1").hide();
-//     $("#question2-2").show();
-//   });
-//
-//   $("#quest1-3").click(function(){
-//     $("#question1").hide();
-//     $("#question2-3").show();
-//   });
-//
-//   $("#quest2-0").click(function(){
-//     $("#question1").hide();
-//     $("#question2-3").show();
-//
-//   });
-// });
+$(document).ready(function(){
+  //Level 1 Buttons
+  $("#quest1-0").click(function(){
+    $("#question1").hide();
+    $("#question2-0").show();
+  });
+  $("#quest1-1").click(function(){
+    $("#question1").hide();
+    $("#question2-1").show();
+  });
+  $("#quest1-2").click(function(){
+    $("#question1").hide();
+    $("#question2-2").show();
+  });
+  $("#quest1-3").click(function(){
+    $("#question1").hide();
+    $("#question2-3").show();
+  });
+
+
+
+  //Level 2 Buttons
+  $("#quest2-0").click(function(){
+    $("#question2-0").hide();
+    $("#question3-0").show();
+  });
+  $("#quest2-1").click(function(){
+    $("#question2-0").hide();
+    $("#question3-1").show();
+  });
+  $("#quest2-2").click(function(){
+    $("#question2-1").hide();
+    $("#question3-2").show();
+  });
+  $("#quest2-3").click(function(){
+    $("#question2-1").hide();
+    $("#question3-3").show();
+  });
+  $("#quest2-4").click(function(){
+    $("#question2-2").hide();
+    $("#question3-4").show();
+  });
+  $("#quest2-5").click(function(){
+    $("#question2-2").hide();
+    $("#question3-5").show();
+  });
+  $("#quest2-6").click(function(){
+    $("#question2-3").hide();
+    $("#question3-6").show();
+  });
+  $("#quest2-7").click(function(){
+    $("#question2-3").hide();
+    $("#question3-7").show();
+  });
+
+  //Level 3 Buttons
+  $("#quest3-0").click(function(){
+    $("#question3-0").hide();
+    $("#question4-0").show();
+  });
+  $("#quest3-1").click(function(){
+    $("#question3-0").hide();
+    $("#question4-1").show();
+  });
+  $("#quest3-2").click(function(){
+    $("#question3-1").hide();
+    $("#question4-2").show();
+  });
+  $("#quest3-3").click(function(){
+    $("#question3-1").hide();
+    $("#question4-3").show();
+  });
+  $("#quest3-4").click(function(){
+    $("#question3-2").hide();
+    $("#question4-4").show();
+  });
+  $("#quest3-5").click(function(){
+    $("#question3-2").hide();
+    $("#question4-5").show();
+  });
+  $("#quest3-6").click(function(){
+    $("#question3-3").hide();
+    $("#question4-6").show();
+  });
+  $("#quest3-7").click(function(){
+    $("#question3-3").hide();
+    $("#question4-7").show();
+  });
+  $("#quest3-8").click(function(){
+    $("#question3-4").hide();
+    $("#question4-8").show();
+  });
+  $("#quest3-9").click(function(){
+    $("#question3-4").hide();
+    $("#question4-9").show();
+  });
+  $("#quest3-10").click(function(){
+    $("#question3-5").hide();
+    $("#question4-10").show();
+  });
+  $("#quest3-11").click(function(){
+    $("#question3-5").hide();
+    $("#question4-11").show();
+  });
+  $("#quest3-12").click(function(){
+    $("#question3-6").hide();
+    $("#question4-12").show();
+  });
+  $("#quest3-13").click(function(){
+    $("#question3-6").hide();
+    $("#question4-13").show();
+  });
+  $("#quest3-14").click(function(){
+    $("#question3-7").hide();
+    $("#question4-14").show();
+  });
+  $("#quest3-15").click(function(){
+    $("#question3-7").hide();
+    $("#question4-15").show();
+  });
+
+
+
+});
